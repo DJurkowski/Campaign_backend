7f07b81a5e318980ec1f893a5116bbff5c3d0eed
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-26 17:06:34 +0200

    Added project and campaign models

---
+++ models/Campaign.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const CampaignSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    keywords: [String],
+    bidAmount: {
+        type: Number,
+        required: true
+    },
+    fund: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true
+    },
+    town: {
+        type: String
+    },
+    radius: {
+        type: Number,
+        required: true
+    },
+    _projectId: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    }
+});
+
+module.exports = Campaign = mongoose.model('campaign', CampaignSchema);
